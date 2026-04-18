b50ea2654bd46f3445c6180376ac9ab390ca5f9d
Vishall77 <Vishalbaghel0777@gmail.com>
2026-04-18 15:01:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,116 @@
+from flask import Flask, render_template, request, redirect, session
+import pickle
+import warnings as w
+w.filterwarnings('ignore')
+
+from src.db import Database
+
+app = Flask(__name__)
+app.secret_key = "supersecretkey"
+obj = Database()
+
+with open('svm.pkl', 'rb') as f:
+    pipe = pickle.load(f)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# Here are register route
+@app.route("/register", methods=['GET', 'POST'])
+def registration():
+    if request.method == 'POST':
+        print("Form Data:", request.form)
+
+        name = request.form.get('name')
+        email_id = request.form.get('email_id')
+        phone_no = request.form.get('phone_no')
+        password = request.form.get('password')
+
+        obj.save_users(name, email_id, phone_no, password)
+
+        msg = 'Your Account is Created Successfully'
+        return render_template('register.html', msg=msg)
+
+    return render_template('register.html')
+
+# Here are login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email_id = request.form.get('email_id')
+        password = request.form.get('password')
+
+        user = obj.check_user(email_id, password)
+
+        if user:
+            session['user'] = user[1]
+            return redirect('/dashboard')
+        else:
+            return render_template('login.html', msg="Invalid Credentials")
+
+    return render_template('login.html')
+
+# Here are dashboard route 
+@app.route('/dashboard')
+def dashboard():
+    if 'user' in session:
+        return render_template('dashboard.html', user=session['user'])
+    return redirect('/login')
+
+
+# Here are predict route
+@app.route('/predict', methods=['GET', 'POST'])
+def prediction():
+
+    if "user" not in session:
+        return redirect('/login')
+    
+    if request.method == 'POST':
+        print("🔍 Prediction Form:", request.form)
+
+        try:
+            Gender = request.form.get('Gender')
+            Married = request.form.get('Married')
+            Dependents = request.form.get('Dependents')
+            Education = request.form.get('Education')
+            Self_Employed = request.form.get('Self_Employed')
+
+            LoanAmount = float(request.form.get('LoanAmount'))
+            Loan_Amount_Term = float(request.form.get('Loan_Amount_Term'))
+            Credit_History = float(request.form.get('Credit_History'))
+            Property_Area = request.form.get('Property_Area')
+            Family_Income = float(request.form.get('Family_Income'))
+
+            Loan_Status = pipe.predict([[
+                Gender, Married, Dependents, Education,
+                Self_Employed, LoanAmount, Loan_Amount_Term,
+                Credit_History, Property_Area, Family_Income
+            ]])[0]
+
+            obj.save_predictions(
+                Gender, Married, Dependents, Education,
+                Self_Employed, LoanAmount, Loan_Amount_Term,
+                Credit_History, Property_Area, Family_Income, Loan_Status
+            )
+
+            return render_template('predict.html', pred=Loan_Status)
+
+        except Exception as e:
+            print("Prediction ERROR:", e)
+            return "Error in prediction"
+
+    return render_template('predict.html')
+
+
+# here are logout route
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/login')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
---
+++ db.py
@@ -0,0 +1,120 @@
+import mysql.connector as sql
+import os
+from dotenv import load_dotenv
+import bcrypt
+
+load_dotenv()
+
+class Database:
+    def __init__(self):
+        self.conn = sql.connect(
+            host=os.getenv("DB_HOST"),
+            port=int(os.getenv("DB_PORT")),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASS"),
+            database=os.getenv('DB_NAME'),
+        )
+        print("Connected DB:", os.getenv("DB_NAME"))
+
+        self.cursor = self.conn.cursor()
+        print("Connection Built to Aiven Successfully")
+
+        # Create tables
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS predictions(
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            Gender VARCHAR(10),
+            Married VARCHAR(10),
+            Dependents VARCHAR(10),
+            Education VARCHAR(20),
+            Self_Employed VARCHAR(10),
+            LoanAmount FLOAT,
+            Loan_Amount_Term FLOAT,
+            Credit_History FLOAT,
+            Property_Area VARCHAR(20),
+            Family_Income FLOAT,
+            Loan_Status VARCHAR(10)
+        )
+        """)
+
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS user_data(
+            user_id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(50),
+            email_id VARCHAR(100) UNIQUE,
+            phone_no VARCHAR(15) UNIQUE,
+            password VARCHAR(200)
+        )
+        """)
+
+        self.conn.commit()
+        print("Tables Created")
+
+    def save_predictions(self, Gender, Married, Dependents, Education,
+                         Self_Employed, LoanAmount, Loan_Amount_Term,
+                         Credit_History, Property_Area, Family_Income, Loan_Status):
+        try:
+            query = """
+            INSERT INTO predictions(
+                Gender, Married, Dependents, Education, Self_Employed,
+                LoanAmount, Loan_Amount_Term, Credit_History,
+                Property_Area, Family_Income, Loan_Status
+            )
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """
+
+            self.cursor.execute(query, (
+                Gender, Married, Dependents, Education,
+                Self_Employed, LoanAmount, Loan_Amount_Term,
+                Credit_History, Property_Area, Family_Income, Loan_Status
+            ))
+
+            self.conn.commit()
+            print("Prediction saved successfully")
+
+        except Exception as e:
+            print("Prediction DB ERROR:", e)
+
+    def save_users(self, name, email_id, phone_no, password):
+        try:
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+            query = """
+            INSERT INTO user_data (name, email_id, phone_no, password)
+            VALUES (%s, %s, %s, %s)
+            """
+            self.cursor.execute(query, (name, email_id, phone_no, hashed_password))
+            self.conn.commit()
+
+            print("User saved successfully")
+
+        except Exception as e:
+            print("User DB ERROR:", e)
+
+    # This is for checking users from database to login
+    def check_user(self, email_id, password):
+        try:
+            query = "SELECT * FROM user_data WHERE email_id = %s"
+            self.cursor.execute(query, (email_id,))
+            user = self.cursor.fetchone()
+
+            if user:
+                stored_password = user[4].encode('utf-8')
+
+                if bcrypt.checkpw(password.encode('utf-8'), stored_password):
+                    return user
+
+            return None
+
+        except Exception as e:
+            print("Login ERROR:", e)
+            return None
+
+
+    # def show_users(self):
+    #     self.cursor.execute("SELECT * FROM user_data")
+    #     self.conn.commit()
+
+
+# obj = Database()
+# obj.show_users()
